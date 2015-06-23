aeb4a57f37dd920fa5002414d74d49a5bab83adc
Marek <marekventur@gmail.com>
2015-06-23 12:41:20 +0100

    Press header

---
+++ gulpfile.js
@@ -0,0 +1,110 @@
+/*eslint-env node */
+"use strict";
+
+var gulp = require("gulp");
+var browserify = require("browserify");
+var babelify = require("babelify");
+var watchify = require("watchify");
+var browserSync = require("browser-sync").create();
+var source = require("vinyl-source-stream");
+var less = require("gulp-less");
+var gutil = require("gulp-util");
+var notify = require("gulp-notify");
+var htmlhint = require("gulp-htmlhint");
+var path = require("path");
+var sourcemaps = require("gulp-sourcemaps");
+var cloudfiles = require("gulp-cloudfiles");
+
+gulp.task("server", function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist/"
+        },
+        open: false
+
+    });
+});
+
+function bundleMainBrowserify(b) {
+    return b.bundle()
+        .on("error", function(err){
+            console.log(err.message);
+        })
+        .pipe(source("main.js"))
+        .pipe(gulp.dest("./dist/"));
+}
+
+function createMainBrowserify() {
+    return browserify({
+        entries: "./js/index.js",
+        transform: [babelify],
+        cache: {},        // for watchify
+        packageCache: {}, // for watchify
+        fullPaths: true
+    });
+}
+
+gulp.task("js", function() {
+    bundleMainBrowserify(createMainBrowserify());
+});
+
+gulp.task("js:watch", function() {
+    var b = watchify(createMainBrowserify());
+    b.on("log", gutil.log);
+    bundleMainBrowserify(b); // run build when watch is started
+    b.on("update", function() {
+        bundleMainBrowserify(b)
+        .pipe(browserSync.stream());
+    });
+});
+
+gulp.task("less", function() {
+    return gulp.src("./less/main.less")
+    .pipe(sourcemaps.init())
+    .pipe(less({
+        paths: [
+            path.normalize(__dirname + "/node_modules")
+        ]
+    }).on("error", gutil.log))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("./dist"))
+    .pipe(browserSync.stream());
+
+});
+
+gulp.task("html", function () {
+    gulp.src("index.html")
+    .pipe(htmlhint())
+    //.pipe(htmlhint.failReporter())
+    .on("error", notify.onError(function(err) {
+        return err.message;
+    }))
+    .pipe(gulp.dest("dist/"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("img", function () {
+    gulp.src(["img/*.png", "img/*.jpg"])
+    .pipe(gulp.dest("dist/img/"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("build", [ "less", "js", "html", "img"]);
+
+gulp.task("watch", ["build", "js:watch"], function () {
+    gulp.watch("index.html", ["html"]);
+    gulp.watch("img/**", ["img"]);
+    gulp.watch("less/**", ["less"]);
+});
+
+gulp.task("deploy", function () {
+    return gulp.src("./dist/**", {read: false})
+        .pipe(cloudfiles({
+            "username": process.env.RAX_USERNAME,
+            "apiKey": process.env.RAX_API_KEY,
+            "region": process.env.RAX_REGION,
+            "container": "gamevy-dot-com"
+        }, {}));
+});
+
+gulp.task("default", ["build", "server", "watch"]);
---
+++ js/landitt.js
@@ -0,0 +1,61 @@
+require("./jquery.waypoints.min.js");
+require("./smoothscroll.js");
+
+/* Auto close navbar on click (mobile menu) */
+$(".navbar-nav > li > a").click(function() {
+    $(".navbar-collapse").collapse('hide');
+});
+
+
+/* Change navbar class on scroll */
+
+$(".wrapper").waypoint(function() {
+    $(".navbar").toggleClass("js-navbar-top");
+    $(".navbar.js-toggleClass").toggleClass("navbar-default navbar-inverse");
+    return false;
+}, { offset: "-20px" });
+
+
+/* Change navbar class on collapse/uncollapse in its top position */
+
+$('.wrapper .navbar-collapse').on('show.bs.collapse', function () {
+    $(".navbar.js-navbar-top").toggleClass("navbar-default navbar-inverse");
+    $(".navbar").toggleClass("js-toggleClass js-noToggleClass");
+});
+
+$('.wrapper .navbar-collapse').on('hide.bs.collapse', function () {
+    $(".navbar.js-navbar-top").toggleClass("navbar-default navbar-inverse");
+    $(".navbar").toggleClass("js-toggleClass js-noToggleClass");
+});
+
+
+/* Sidebar */
+
+$(".js-toggle-sidebar").on('click', function() {
+    $(".wrapper").toggleClass("js-wrapper-aside");
+    $(".navbar").toggleClass("js-navbar-aside");
+    $(".sidebar").toggleClass("js-sidebar-aside");
+    return false;
+});
+
+
+/**
+ * Smooth scroll to anchor
+ */
+
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: (target.offset().top - 70) // 70px offset for navbar menu
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+
