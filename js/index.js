b82a0a93940d56bb0cbd6c947e44067b6b02cf59
Marek <marekventur@gmail.com>
2015-06-25 16:00:21 +0100

    restructure js, use npm for everything

--- js/index.js
+++ js/index.js
@@ -1,3 +1,51 @@
-window.jQuery = window.$ = require("jquery");
+import $ from "jquery"
+window.jQuery = $;
 require("bootstrap");
-require("./landitt");
+
+require("../node_modules/waypoints/lib/jquery.waypoints.js");
+
+if ($('.hero').length) {
+    /* Auto close navbar on click (mobile menu) */
+    $(".navbar-nav > li > a").click(function() {
+        $(".navbar-collapse").collapse('hide');
+        console.log("here");
+    });
+
+    /* Change navbar class on scroll */
+    $(".wrapper").waypoint(function() {
+        $(".navbar").toggleClass("js-navbar-top");
+        $(".navbar.js-toggleClass").toggleClass("navbar-default navbar-inverse");
+        return false;
+    }, {offset: "-20px" });
+
+
+    /* Change navbar class on collapse/uncollapse in its top position */
+    $('.wrapper .navbar-collapse').on('show.bs.collapse', function () {
+        $(".navbar.js-navbar-top").toggleClass("navbar-default navbar-inverse");
+        $(".navbar").toggleClass("js-toggleClass js-noToggleClass");
+    });
+
+    $('.wrapper .navbar-collapse').on('hide.bs.collapse', function () {
+        $(".navbar.js-navbar-top").toggleClass("navbar-default navbar-inverse");
+        $(".navbar").toggleClass("js-toggleClass js-noToggleClass");
+    });
+
+    /* Smooth scroll to anchor */
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: (target.offset().top - 70) // 70px offset for navbar menu
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+} else {
+    $(".navbar").toggleClass("js-navbar-top");
+    $(".navbar.js-toggleClass").toggleClass("navbar-default navbar-inverse");
+}
