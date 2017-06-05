1f4918bd9f111a40900f69d5fafb5f8e0288b1c5
Dan Rough <dan.rough@gmail.com>
2017-06-05 15:06:01 +0100

    Corrected the game id for Tennis Scratch

--- js/kiosk.js
+++ js/kiosk.js
@@ -19,7 +19,7 @@ const GAMES = [
     { image: "treasure_scratch", gameId: "scratch", variation: "&gameId=BETSSON_TREASURE&brand=betsson" },
     { image: "diamond_scratch", gameId: "scratch", variation: "&gameId=BETSSON_DIAMOND&brand=betsson" },
     { image: "7_11_21", gameId: "7-11-21" },
-    { image: "tennis", gameId: "scratch", variation: "&gameId=TENNIS" },
+    { image: "tennis", gameId: "scratch", variation: "&gameId=TENNIS_SCRATCH" },
     { image: "football", gameId: "scratch", variation: "&gameId=FOOTBALL" },
     { image: "gold_cup", gameId: "scratch", variation: "&gameId=GOLD_CUP" },
 ];
